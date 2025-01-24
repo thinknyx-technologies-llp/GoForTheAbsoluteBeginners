207534847e0052720db31e6943c3d528ebcd3de5
Dheeraj Sain <142474250+dheeraj-sain@users.noreply.github.com>
2025-01-24 15:42:48 +0530

    Update Demo4_File_handling_2.go

--- Section_8_Core_Packages/Demo4_File_handling_2.go
+++ Section_8_Core_Packages/Demo4_File_handling_2.go
@@ -3,7 +3,7 @@ import (
 	"fmt"
 	"os")
 func demo3(){
-	file, err := os.OpenFile("test.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0600)
+	file, err := os.OpenFile("test.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0600) // os.O_WRONLY is write-read only mode
 	if err != nil{
 		fmt.Println(err)
 		return
